dd61be834cbf4a0b3f4e0993cdadbdd9b9d8e4c4
Kyle Yusong Yao <kyusongy@unc.edu>
2024-02-25 12:49:49 -0500

    ex04gai

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,25 +5,29 @@ __author__: str = "730720425"
 
 def all(x: list[int], nbr: int) -> bool:
     """Return a bool indicating whether or not all the ints in the list are the same as the given int."""
-    count: int = 0
-    for i in x:
-        if i == nbr:
-            count += 1
-    if count == len(x):
-        return True
-    else:
+    if len(x) == 0:
         return False
+    else:
+        count: int = 0
+        for i in x:
+            if i == nbr:
+                count += 1
+        if count == len(x):
+            return True
+        else:
+            return False
 
 
 def max(x: list[int]) -> int:
     """Return the largest in the List."""
     if len(x) == 0:
         raise ValueError("max() arg is an empty List")
-    temp_max: int = 0
-    for i in x:
-        if i >= temp_max:
-            temp_max = i
-    return temp_max
+    else:
+        temp_max: int = x[0] - 1
+        for i in x:
+            if i >= temp_max:
+                temp_max = i
+        return temp_max
 
 
 def is_equal(x: list[int], y: list[int]) -> bool:
@@ -31,7 +35,7 @@ def is_equal(x: list[int], y: list[int]) -> bool:
     if len(x) != len(y):
         return False
     else:
-        count:int = 0
+        count: int = 0
         idx_x: int = 0
         while idx_x < len(x):
             for idx_y in range(0, len(y)):
@@ -46,6 +50,6 @@ def is_equal(x: list[int], y: list[int]) -> bool:
 
 
 def extend(x: list[int], y: list[int]) -> None:
-    """Mutate the first list of int values by appending the second lists values to the end of it. """
+    """Mutate the first list of int values by appending the second lists values to the end of it."""
     for i in y:
         x.append(i)
